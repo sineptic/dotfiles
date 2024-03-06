a225d461400b6482d5cad873ca31f4ffbcbab42f
sineptic <sineptic0@gmail.com>
2024-03-06 18:36:13 +0300

    add install script

---
+++ install_script.fish
@@ -0,0 +1,31 @@
+#!/usr/bin/env fish
+
+function get_user_agreement -d "user must write 'y'(return 1) or 'n'(return 0)"
+  set -l input ""
+
+  while test $input != "y" 
+    and test $input != "n"
+  
+    printf "('y'es or 'n'o): \n"
+    read -f -p "printf '\e[2K\e[1A\e[15C             '" input
+  end
+  
+  if test $input = "y"
+    return 1
+  else
+    return 0
+  end
+end
+
+set -l programs {
+"neovim", 
+"zellij"
+}
+set -l programs_as_string $(for program in $programs; echo $(string trim $program); end | string collect | string split "\n" | string join " ")
+
+echo "Install programs?"
+get_user_agreement
+if test $status -eq 1
+  printf "Root " # root password
+  command su -c "pacman -Sy --needed $programs_as_string"
+end
