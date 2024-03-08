ac574ace0c35898b190e9da21203c0211a9f7868
sineptic <sineptic0@gmail.com>
2024-03-08 22:33:12 +0300

    execute all commands than needs root access onve

--- install_script.fish
+++ install_script.fish
@@ -1,12 +1,14 @@
 #!/usr/bin/env fish
 
+set root_commands
+
 function get_user_agreement -d "user must write 'y'(return 1) or 'n'(return 0)"
   set -l input ""
 
   while test $input != "y" 
     and test $input != "n"
   
-    printf "('y'es or 'n'o): \n"
+    printf "\n('y'es or 'n'o): \n"
     read -f -p "printf '\e[2K\e[1A\e[15C             '" input
   end
   
@@ -37,31 +39,40 @@ set -l fonts {
 "ttf-monofur-nerd",
 "ttf-jetbrains-mono-nerd"
 }
-set -l programs_as_string $(for program in $programs; echo $(string trim $program); end | string collect | string split "\n" | string join " ")
-set -l fonts_as_string $(for font in $fonts; echo $(string trim $font); end | string collect | string split "\n" | string join " ")
+set programs_as_string $(for program in $programs; echo $(string trim $program); end | string collect | string split "\n" | string join " ")
+set fonts_as_string $(for font in $fonts; echo $(string trim $font); end | string collect | string split "\n" | string join " ")
 
 
-echo "Install programs?"
+echo -n "Install programs?"
 get_user_agreement
 if test $status -eq 1
-  printf "Root " # root password
-  command su -c "pacman -Sy --needed $programs_as_string"
+  set -a root_commands "pacman -Sy --needed $programs_as_string"
 end
 
 printf "\n"
 
-echo "Install fonts?"
+echo -n "Install fonts?"
 get_user_agreement
 if test $status -eq 1
-  printf "Root " # root password
-  command su -c "pacman -Sy --needed $fonts_as_string"
+  set -a root_commands "pacman -Sy --needed $programs_as_string"
 end
 
 printf "\n"
 
-echo "Install dotfiles?"
+echo -n "Install dotfiles?"
 get_user_agreement
 if test $status -eq 1
   # TODO
   echo "ERROR: unimplemented"
 end
+
+
+# execute root root commands
+set root_commands_as_string 
+if fish_is_root_user
+  command "$(string join -n "; " $root_commands)"
+else
+  printf "Root " # root password
+  command su -c "$(string join -n "; " $root_commands)"
+end
+
