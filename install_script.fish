cd091d1cfeba27c01242a6b4e8c18b38255205d3
sineptic <sineptic0@gmail.com>
2024-03-10 12:23:22 +0300

    add fish dotfiles

--- install_script.fish
+++ install_script.fish
@@ -1,4 +1,4 @@
-#!/usr/bin/env fish
+j!/usr/bin/env fish
 
 set root_commands
 
@@ -125,6 +125,11 @@ end
 
 function fzf_theming
   set -Ux FZF_DEFAULT_OPTS "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+end
+
+function fish_dotfiles
+  command_silent ln -sf "$dotfiles_dir/fish" "$HOME/.config/"
+  echo "fish configuration is finished"
 end
 
 echo -n "Install dotfiles?"
