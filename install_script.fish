a3441b26edcca54ee3e1bdfcd8b9d9f2c15e14ef
sineptic <sineptic0@gmail.com>
2024-03-10 11:42:34 +0300

    add fzf theming

--- install_script.fish
+++ install_script.fish
@@ -123,6 +123,10 @@ function starship_dotfiles
   echo "starship configuration is finished"
 end
 
+function fzf_theming
+  set -Ux FZF_DEFAULT_OPTS "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+end
+
 echo -n "Install dotfiles?"
 get_user_agreement
 if test $status -eq 1
@@ -130,5 +134,6 @@ if test $status -eq 1
   btop_dotfiles
   starship_dotfiles
   fish_theming
+  fzf_theming
   echo "configuration completed"
 end
