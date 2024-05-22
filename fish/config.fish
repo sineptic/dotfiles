479d9a4e2b1466bd8ac5542625de7d92d3e1ce70
sineptic <sineptic0@gmail.com>
2024-05-22 10:58:46 +0300

    format file

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,26 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-  function ls -w exa 
-    command exa $argv
-  end
-  function ll -w exa
-    command exa --long --no-user --no-time --smart-group $argv
-  end
-  function lls -w exa 
-    ll --total-size $argv
-  end
-
-  function tree -w exa 
-    command exa --tree $argv
-  end
-  function ftree -w exa 
-    tree --level 1 $argv
-  end
-  function suc -w command -d 'alisas for su -c "argv"'
-    command su -c "$argv"
-  end
+      # Commands to run in interactive sessions can go here
+    function ls -w exa 
+        command exa $argv
+    end
+    function ll -w exa
+        command exa --long --no-user --no-time --smart-group $argv
+    end
+    function lls -w exa 
+        ll --total-size $argv
+    end 
+    function tree -w exa 
+        command exa --tree $argv
+    end
+    function ftree -w exa 
+        tree --level 1 $argv
+    end
+    function suc -w command -d 'alisas for su -c "argv"'
+        command su -c "$argv"
+    end
+    function lazygit -w lazygit
+        command lazygit --use-config-file="$(command lazygit -cd)/config.yml,$(command lazygit -cd)/catppuccin/themes-mergable/mocha/mauve.yml"
+    end
 
   starship init fish | source
   zoxide init --cmd cd fish | source
